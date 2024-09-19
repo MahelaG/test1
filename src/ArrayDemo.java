985f289973c1a4c514cc19c7929778a48cab3037
Mahela <lenken125@gmail.com>
2024-09-19 19:35:37 +1000

    1 commit

---
+++ src/ArrayDemo.java
@@ -0,0 +1,23 @@
+public class ArrayDemo {
+    private int id;
+    private String name;
+    private String specialization;
+
+    public ArrayDemo() {
+        this.id = 0;
+        this.name = "Unknown";
+        this.specialization = "Unknown";
+    }
+
+    public ArrayDemo(int id, String name, String specialization) {
+        this.id = id;
+        this.name = name;
+        this.specialization = specialization;
+    }
+
+    public void printDetails() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Specialization: " + specialization);
+    }
+}
